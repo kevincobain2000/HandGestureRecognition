4bafe21873796778f84971fb2bd040c8f6dd7f56
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2013-11-02 13:57:46 +0900

    initial commit

---
+++ HandGestureRecognition/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace HandGestureRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageBoxFrameGrabber = new Emgu.CV.UI.ImageBox();
+            this.splitContainerFrames = new System.Windows.Forms.SplitContainer();
+            this.imageBoxSkin = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).BeginInit();
+            this.splitContainerFrames.Panel1.SuspendLayout();
+            this.splitContainerFrames.Panel2.SuspendLayout();
+            this.splitContainerFrames.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxSkin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBoxFrameGrabber
+            // 
+            this.imageBoxFrameGrabber.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBoxFrameGrabber.Location = new System.Drawing.Point(0, 0);
+            this.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber";
+            this.imageBoxFrameGrabber.Size = new System.Drawing.Size(324, 391);
+            this.imageBoxFrameGrabber.TabIndex = 2;
+            this.imageBoxFrameGrabber.TabStop = false;
+            // 
+            // splitContainerFrames
+            // 
+            this.splitContainerFrames.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerFrames.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerFrames.Name = "splitContainerFrames";
+            // 
+            // splitContainerFrames.Panel1
+            // 
+            this.splitContainerFrames.Panel1.Controls.Add(this.imageBoxFrameGrabber);
+            // 
+            // splitContainerFrames.Panel2
+            // 
+            this.splitContainerFrames.Panel2.Controls.Add(this.imageBoxSkin);
+            this.splitContainerFrames.Size = new System.Drawing.Size(685, 391);
+            this.splitContainerFrames.SplitterDistance = 324;
+            this.splitContainerFrames.TabIndex = 3;
+            // 
+            // imageBoxSkin
+            // 
+            this.imageBoxSkin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBoxSkin.Location = new System.Drawing.Point(0, 0);
+            this.imageBoxSkin.Name = "imageBoxSkin";
+            this.imageBoxSkin.Size = new System.Drawing.Size(357, 391);
+            this.imageBoxSkin.TabIndex = 2;
+            this.imageBoxSkin.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 391);
+            this.Controls.Add(this.splitContainerFrames);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).EndInit();
+            this.splitContainerFrames.Panel1.ResumeLayout(false);
+            this.splitContainerFrames.Panel2.ResumeLayout(false);
+            this.splitContainerFrames.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxSkin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBoxFrameGrabber;
+        private System.Windows.Forms.SplitContainer splitContainerFrames;
+        private Emgu.CV.UI.ImageBox imageBoxSkin;
+    }
+}
+
---
+++ HandGestureRecognition/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using HandGestureRecognition.SkinDetector;
+
+namespace HandGestureRecognition
+{
+    public partial class Form1 : Form
+    {
+
+        IColorSkinDetector skinDetector;
+
+        Image<Bgr, Byte> currentFrame;
+        Image<Bgr, Byte> currentFrameCopy;
+                
+        Capture grabber;
+        AdaptiveSkinDetector detector;
+        
+        int frameWidth;
+        int frameHeight;
+        
+        Hsv hsv_min;
+        Hsv hsv_max;
+        Ycc YCrCb_min;
+        Ycc YCrCb_max;
+        
+        Seq<Point> hull;
+        Seq<Point> filteredHull;
+        Seq<MCvConvexityDefect> defects;
+        MCvConvexityDefect[] defectArray;
+        Rectangle handRect;
+        MCvBox2D box;
+        Ellipse ellip;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            grabber = new Emgu.CV.Capture(@".\..\..\..\M2U00253.MPG");            
+            grabber.QueryFrame();
+            frameWidth = grabber.Width;
+            frameHeight = grabber.Height;            
+            detector = new AdaptiveSkinDetector(1, AdaptiveSkinDetector.MorphingMethod.NONE);
+            hsv_min = new Hsv(0, 45, 0); 
+            hsv_max = new Hsv(20, 255, 255);            
+            YCrCb_min = new Ycc(0, 131, 80);
+            YCrCb_max = new Ycc(255, 185, 135);
+            box = new MCvBox2D();
+            ellip = new Ellipse();
+
+            Application.Idle += new EventHandler(FrameGrabber);                        
+        }
+
+        void FrameGrabber(object sender, EventArgs e)
+        {
+            currentFrame = grabber.QueryFrame();
+            if (currentFrame != null)
+            {
+                currentFrameCopy = currentFrame.Copy();
+                
+                // Uncomment if using opencv adaptive skin detector
+                //Image<Gray,Byte> skin = new Image<Gray,byte>(currentFrameCopy.Width,currentFrameCopy.Height);                
+                //detector.Process(currentFrameCopy, skin);                
+
+                skinDetector = new YCrCbSkinDetector();
+                
+                Image<Gray, Byte> skin = skinDetector.DetectSkin(currentFrameCopy,YCrCb_min,YCrCb_max);
+
+                ExtractContourAndHull(skin);
+                                
+                DrawAndComputeFingersNum();
+
+                imageBoxSkin.Image = skin;
+                imageBoxFrameGrabber.Image = currentFrame;
+            }
+        }
+               
+        private void ExtractContourAndHull(Image<Gray, byte> skin)
+        {
+            using (MemStorage storage = new MemStorage())
+            {
+
+                Contour<Point> contours = skin.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_LIST, storage);
+                Contour<Point> biggestContour = null;
+
+                Double Result1 = 0;
+                Double Result2 = 0;
+                while (contours != null)
+                {
+                    Result1 = contours.Area;
+                    if (Result1 > Result2)
+                    {
+                        Result2 = Result1;
+                        biggestContour = contours;
+                    }
+                    contours = contours.HNext;
+                }
+
+                if (biggestContour != null)
+                {
+                    //currentFrame.Draw(biggestContour, new Bgr(Color.DarkViolet), 2);
+                    Contour<Point> currentContour = biggestContour.ApproxPoly(biggestContour.Perimeter * 0.0025, storage);
+                    currentFrame.Draw(currentContour, new Bgr(Color.LimeGreen), 2);
+                    biggestContour = currentContour;
+                
+
+                    hull = biggestContour.GetConvexHull(Emgu.CV.CvEnum.ORIENTATION.CV_CLOCKWISE);
+                    box = biggestContour.GetMinAreaRect();
+                    PointF[] points = box.GetVertices();
+                    //handRect = box.MinAreaRect();
+                    //currentFrame.Draw(handRect, new Bgr(200, 0, 0), 1);
+
+                    Point[] ps = new Point[points.Length];
+                    for (int i = 0; i < points.Length; i++)
+                        ps[i] = new Point((int)points[i].X, (int)points[i].Y);
+
+                    currentFrame.DrawPolyline(hull.ToArray(), true, new Bgr(200, 125, 75), 2);
+                    currentFrame.Draw(new CircleF(new PointF(box.center.X, box.center.Y), 3), new Bgr(200, 125, 75), 2);
+
+                    //ellip.MCvBox2D= CvInvoke.cvFitEllipse2(biggestContour.Ptr);
+                    //currentFrame.Draw(new Ellipse(ellip.MCvBox2D), new Bgr(Color.LavenderBlush), 3);
+
+                    PointF center;
+                    float radius;
+                    //CvInvoke.cvMinEnclosingCircle(biggestContour.Ptr, out  center, out  radius);
+                    //currentFrame.Draw(new CircleF(center, radius), new Bgr(Color.Gold), 2);
+
+                    //currentFrame.Draw(new CircleF(new PointF(ellip.MCvBox2D.center.X, ellip.MCvBox2D.center.Y), 3), new Bgr(100, 25, 55), 2);
+                    //currentFrame.Draw(ellip, new Bgr(Color.DeepPink), 2);
+
+                    //CvInvoke.cvEllipse(currentFrame, new Point((int)ellip.MCvBox2D.center.X, (int)ellip.MCvBox2D.center.Y), new System.Drawing.Size((int)ellip.MCvBox2D.size.Width, (int)ellip.MCvBox2D.size.Height), ellip.MCvBox2D.angle, 0, 360, new MCvScalar(120, 233, 88), 1, Emgu.CV.CvEnum.LINE_TYPE.EIGHT_CONNECTED, 0);
+                    //currentFrame.Draw(new Ellipse(new PointF(box.center.X, box.center.Y), new SizeF(box.size.Height, box.size.Width), box.angle), new Bgr(0, 0, 0), 2);
+
+
+                    filteredHull = new Seq<Point>(storage);
+                    for (int i = 0; i < hull.Total; i++)
+                    {
+                        if (Math.Sqrt(Math.Pow(hull[i].X - hull[i + 1].X, 2) + Math.Pow(hull[i].Y - hull[i + 1].Y, 2)) > box.size.Width / 10)
+                        {
+                            filteredHull.Push(hull[i]);
+                        }
+                    }
+
+                    defects = biggestContour.GetConvexityDefacts(storage, Emgu.CV.CvEnum.ORIENTATION.CV_CLOCKWISE);
+
+                    defectArray = defects.ToArray();
+                }
+            }
+        }
+
+        private void DrawAndComputeFingersNum()
+        {
+            int fingerNum = 0;
+
+            #region hull drawing
+            //for (int i = 0; i < filteredHull.Total; i++)
+            //{
+            //    PointF hullPoint = new PointF((float)filteredHull[i].X,
+            //                                  (float)filteredHull[i].Y);
+            //    CircleF hullCircle = new CircleF(hullPoint, 4);
+            //    currentFrame.Draw(hullCircle, new Bgr(Color.Aquamarine), 2);
+            //}
+            #endregion
+
+            #region defects drawing
+            for (int i = 0; i < defects.Total; i++)
+            {
+                PointF startPoint = new PointF((float)defectArray[i].StartPoint.X,
+                                                (float)defectArray[i].StartPoint.Y);
+
+                PointF depthPoint = new PointF((float)defectArray[i].DepthPoint.X,
+                                                (float)defectArray[i].DepthPoint.Y);
+
+                PointF endPoint = new PointF((float)defectArray[i].EndPoint.X,
+                                                (float)defectArray[i].EndPoint.Y);
+
+                LineSegment2D startDepthLine = new LineSegment2D(defectArray[i].StartPoint, defectArray[i].DepthPoint);
+
+                LineSegment2D depthEndLine = new LineSegment2D(defectArray[i].DepthPoint, defectArray[i].EndPoint);
+
+                CircleF startCircle = new CircleF(startPoint, 5f);
+
+                CircleF depthCircle = new CircleF(depthPoint, 5f);
+
+                CircleF endCircle = new CircleF(endPoint, 5f);
+
+                //Custom heuristic based on some experiment, double check it before use
+                if ((startCircle.Center.Y < box.center.Y || depthCircle.Center.Y < box.center.Y) && (startCircle.Center.Y < depthCircle.Center.Y) && (Math.Sqrt(Math.Pow(startCircle.Center.X - depthCircle.Center.X, 2) + Math.Pow(startCircle.Center.Y - depthCircle.Center.Y, 2)) > box.size.Height / 6.5))
+                {
+                    fingerNum++;
+                    currentFrame.Draw(startDepthLine, new Bgr(Color.Green), 2);
+                    //currentFrame.Draw(depthEndLine, new Bgr(Color.Magenta), 2);
+                }
+
+                                
+                currentFrame.Draw(startCircle, new Bgr(Color.Red), 2);
+                currentFrame.Draw(depthCircle, new Bgr(Color.Yellow), 5);
+                //currentFrame.Draw(endCircle, new Bgr(Color.DarkBlue), 4);
+            }
+            #endregion
+
+            MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_DUPLEX, 5d, 5d);
+            currentFrame.Draw(fingerNum.ToString(), ref font, new Point(50, 150), new Bgr(Color.White));
+        }
+                                      
+    }
+}
